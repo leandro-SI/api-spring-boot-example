bcfc6c778d9cad94a4fc1189907e4c1fcae7a26e
Leandro <leandro.almeida@uvvnet.com.br>
2023-10-28 17:44:05 -0300

    metodos http

--- src/main/java/com/example/springboot/controllers/ProdutoController.java
+++ src/main/java/com/example/springboot/controllers/ProdutoController.java
@@ -1,11 +1,20 @@
 package com.example.springboot.controllers;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.springboot.dtos.ProdutoRecordDto;
@@ -13,6 +22,7 @@ import com.example.springboot.models.ProdutoModel;
 import com.example.springboot.repositories.ProdutoRepository;
 
 import jakarta.validation.Valid;
+import jakarta.websocket.server.PathParam;
 
 @RestController
 public class ProdutoController {
@@ -28,4 +38,61 @@ public class ProdutoController {
 		return ResponseEntity.status(HttpStatus.CREATED).body(produtoRepository.save(produtoModel));
 		
 	}
+	
+	@GetMapping("/produtos")
+	public ResponseEntity<List<ProdutoModel>> ListarTodos() {
+		
+		return ResponseEntity.status(HttpStatus.OK).body(produtoRepository.findAll());
+	}
+	
+	@GetMapping("/produto/{id}")
+	public ResponseEntity<Object> Buscar(@PathVariable(value="id") UUID id) {
+		
+		Optional<ProdutoModel> produto = produtoRepository.findById(id);
+		
+		if (produto.isEmpty())
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Produto não encontrado");
+		
+		return ResponseEntity.status(HttpStatus.OK).body(produto.get());
+	}
+	
+	@PutMapping("produto/{id}")
+	public ResponseEntity<Object> Atualizar(@PathVariable(value="id") UUID id, @RequestBody @Valid ProdutoRecordDto dto) {
+		
+		Optional<ProdutoModel> produtoO = produtoRepository.findById(id);
+		
+		if (produtoO.isEmpty())
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Produto não encontrado");
+		
+		var produtoModel = produtoO.get();
+		
+		BeanUtils.copyProperties(dto, produtoModel);		
+		
+		return ResponseEntity.status(HttpStatus.OK).body(produtoRepository.save(produtoModel));
+	}
+	
+	@DeleteMapping("produto/{id}") 
+	public ResponseEntity<Object> Excluir(@PathVariable(value="id") UUID id) {
+		
+		Optional<ProdutoModel>  produtoO = produtoRepository.findById(id);
+		
+		if (produtoO.isEmpty())
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Produto não encontrado");
+		
+		produtoRepository.delete(produtoO.get());	
+		
+		return ResponseEntity.status(HttpStatus.OK).body("Produto excluido com sucesso");
+	}
+	
+	
 }
+
+
+
+
+
+
+
+
+
+
